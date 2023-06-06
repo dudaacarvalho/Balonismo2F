9ce4ed80c8becf60ce52cc2157fa08882b2abd9a
dudaacarvalho <eduarda.mecap@gmail.com>
2023-06-06 08:30:05 -0300

    Iniciamos o projeto balonismo, criamos as classes e os atributos.

--- src/Balao.java
+++ src/Balao.java
@@ -1,2 +1,18 @@
 public class Balao {
+
+    // atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+
+    // metodos
+    public String voar () {
+        return "voando";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // construir baloes
+        Balao b1 = new Balao();
+        String resultado = b1.voar();
+        System.out.println(resultado);
     }
 }
